01e357f88e4629ef49fe1b1813689aaecd70905f
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-22 18:34:14 +0200

    Added Autofac

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -1,6 +1,8 @@
 using API.Configuration.Extensions;
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +31,13 @@ namespace API
             services.AddControllers();
 
             services.AddSwaggerDocumentation();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        }
+        
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
