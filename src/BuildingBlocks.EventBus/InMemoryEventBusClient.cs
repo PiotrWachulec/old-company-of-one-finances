98e40135dbd8622a115cf58126bdd0d0484b8fcb
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-23 19:28:06 +0200

    Added EventBus client

---
+++ src/BuildingBlocks.EventBus/InMemoryEventBusClient.cs
@@ -0,0 +1,31 @@
+using BuildingBlocks.Infrastructure.EventBus;
+using Serilog;
+
+namespace BuildingBlocks.EventBus
+{
+    public class InMemoryEventBusClient : IEventBus
+    {
+        private readonly ILogger _logger;
+
+        public InMemoryEventBusClient(ILogger logger)
+        {
+            _logger = logger;
+        }
+        
+        public void Publish<T>(T @event) where T : IntegrationEvent
+        {
+            _logger.Information("Publishing {Event}", @event.GetType().FullName);
+            InMemoryEventBus.Instance.Publish(@event);
+        }
+
+        public void Subscribe<T>(IIntegrationEventHandler<T> handler) where T : IntegrationEvent
+        {
+            InMemoryEventBus.Instance.Subscribe(handler);
+        }
+        
+        public void Dispose()
+        {
+            
+        }
+    }
+}
