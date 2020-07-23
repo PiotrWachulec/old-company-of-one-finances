55b0921e416c0ddb2d7cb233532c0ec3ae7bed34
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-23 20:12:53 +0200

    Added Inbox message - Inbox/Outbox pattern

---
+++ src/BuildingBlocks.Infrastructure/Inbox/InboxMessage.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace BuildingBlocks.Infrastructure.Inbox
+{
+    public class InboxMessage
+    {
+        public Guid Id { get; set; }
+
+        public DateTime OccurredOn { get; set; }
+
+        public string Type { get; set; }
+
+        public string Data { get; set; }
+
+        public DateTime? ProcessedDate { get; set; }
+
+        private InboxMessage()
+        {
+            
+        }
+
+        public InboxMessage(DateTime occurredOn, string type, string data)
+        {
+            Id = Guid.NewGuid();
+            OccurredOn = occurredOn;
+            Type = type;
+            Data = data;
+        }
+    }
+}
