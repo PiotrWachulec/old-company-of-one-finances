27c1ad3ab6c0437de0bc4727507d01428bb52ed3
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-27 13:47:06 +0200

    Added command handlers decorators
    For logging, unit of work and validation

---
+++ src/Modules.UserAccess.Infrastructure/Configuration/Processing/Decorators/Validation/ValidationCommandHandlerDecorator.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using BuildingBlocks.Application;
+using FluentValidation;
+using MediatR;
+using Modules.UserAccess.Application.Configuration.Commands;
+using Modules.UserAccess.Application.Contracts;
+
+namespace Modules.UserAccess.Infrastructure.Configuration.Processing.Decorators.Validation
+{
+    internal class ValidationCommandHandlerDecorator<T> : ICommandHandler<T> where T : ICommand
+    {
+        private readonly IList<IValidator<T>> _validators;
+        private readonly ICommandHandler<T> _decorated;
+
+        public ValidationCommandHandlerDecorator(
+            IList<IValidator<T>> validators, 
+            ICommandHandler<T> decorated)
+        {
+            _validators = validators;
+            _decorated = decorated;
+        }
+
+        public Task<Unit> Handle(T command, CancellationToken cancellationToken)
+        {
+            var errors = _validators
+                .Select(v => v.Validate(command))
+                .SelectMany(result => result.Errors)
+                .Where(error => error != null)
+                .ToList();
+
+            if (errors.Any())
+            {
+                throw new InvalidCommandException(errors.Select(x => x.ErrorMessage).ToList());
+            }
+
+            return _decorated.Handle(command, cancellationToken);
+        }
+    }
+}
