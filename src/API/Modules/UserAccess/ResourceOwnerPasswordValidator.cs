1c61f9dfade8e24e2698d37b7e82bdc9c37bb893
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-28 13:28:16 +0200

    Created password validator for IdentityServer

---
+++ src/API/Modules/UserAccess/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Validation;
+using Modules.UserAccess.Application.Authentication.Authenticate;
+using Modules.UserAccess.Application.Contracts;
+
+namespace API.Modules.UserAccess
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        private readonly IUserAccessModule _userAccessModule;
+
+        public ResourceOwnerPasswordValidator(IUserAccessModule userAccessModule)
+        {
+            _userAccessModule = userAccessModule;
+        }
+
+        public async Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            var authenticationResult = await _userAccessModule.ExecuteCommandAsync(
+                new AuthenticateCommand(context.UserName, context.Password));
+
+            if (!authenticationResult.IsAuthenticated)
+            {
+                context.Result = new GrantValidationResult(
+                    TokenRequestErrors.InvalidGrant, 
+                    authenticationResult.AuthenticationError);
+
+                return;
+            }
+
+            context.Result = new GrantValidationResult(
+                authenticationResult.User.Id.ToString(), 
+                "forms", 
+                authenticationResult.User.Claims);
+        }
+    }
+}
