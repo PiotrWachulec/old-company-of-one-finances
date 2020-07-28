e53654394ca01a84aa155fb48e0e75f8df71aaa0
Piotr Wachulec <p.wachulec@gmail.com>
2020-07-28 19:38:01 +0200

    Added implementation of UserRegistrationConfirmed notification

--- src/Modules.UserAccess.Application/Users/CreateUser/UserRegistrationConfirmedNotificationHandler.cs
+++ src/Modules.UserAccess.Application/Users/CreateUser/UserRegistrationConfirmedNotificationHandler.cs
@@ -1,14 +1,25 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Modules.UserAccess.Application.Configuration.Commands;
 
 namespace Modules.UserAccess.Application.Users.CreateUser
 {
     public class UserRegistrationConfirmedNotificationHandler : INotificationHandler<UserRegistrationConfirmedNotification>
     {
+        private readonly ICommandsScheduler _commandsScheduler;
+
+        public UserRegistrationConfirmedNotificationHandler(ICommandsScheduler commandsScheduler)
+        {
+            _commandsScheduler = commandsScheduler;
+        }
+        
         public Task Handle(UserRegistrationConfirmedNotification notification, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            _commandsScheduler.EnqueueAsync(new CreateUserCommand(Guid.NewGuid(), notification.DomainEvent.UserRegistrationId));
+
+            return Task.CompletedTask;
         }
     }
 }
