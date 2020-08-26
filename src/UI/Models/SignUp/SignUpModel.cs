93dbafa5fa0a3c66a9a769b5d7978e701b7ede64
Piotr Wachulec <p.wachulec@gmail.com>
2020-08-26 09:39:12 +0200

    Created signUp model

---
+++ src/UI/Models/SignUp/SignUpModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UI.Models.SignUp
+{
+    public class SignUpModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
